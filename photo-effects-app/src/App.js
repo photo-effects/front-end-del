59148e1d48f8e5f83f13c24a7f7e96f7d102d4a3
Edgar F <eflores.ef@gmail.com>
2019-08-19 20:32:26 -0700

    Set up axios.get call to read sample data from backend api.

--- photo-effects-app/src/App.js
+++ photo-effects-app/src/App.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div>
-      Photo Effects App
-    </div>
-  );
+class App extends Component {
+  state = {
+    testData: []
+  }
+
+  componentDidMount() {
+    // axios.get("https://swapi.co/api/people")
+    axios.get("")
+    .then(res => this.setState({testData: res.data.results}))
+    .catch(err => console.log(err))
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Photo Effects</h1>
+        {this.state.testData.map(data => {
+          return <div>{data.name}</div>
+        })}
+      </div>
+    );
+  }
+    
 }
 
 export default App;
