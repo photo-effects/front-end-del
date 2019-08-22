96950a83fd3fa272eceeef26739874c4bab3e377
Edgar F <eflores.ef@gmail.com>
2019-08-21 20:49:23 -0700

    ready for testing

--- photo-effects-app/src/App.js
+++ photo-effects-app/src/App.js
@@ -1,33 +1,15 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+import { Route } from 'react-router-dom';
+import Users from './components/Users';
 
-class App extends Component {
-  state = {
-    testData: []
-  }
-
-  componentDidMount() {
-    axios.get("https://photo-effect-backend.herokuapp.com/api/users")
-    .then(res => this.setState({testData: res.data}))
-    .catch(err => console.log(err))
-  }
-
-  render() {
-    return (
-      <div>
-        <h1>Photo Effects</h1>
-        {this.state.testData.map(data => {
-          return (
-            <>
-              <div>{data.id}</div>
-              <div>{data.username}</div>
-              <div>{data.password}</div>
-            </>
-          )
-        })}
-      </div>
-    );
-  }
+function App() {
+ 
+  return (
+    <div>
+      <h1>Photo Effects</h1>
+      <Users />
+    </div>
+  );
 }
 
 export default App;
---
+++ photo-effects-app/src/components/Users.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class Users extends Component {
+   state = {
+      testUsers: []
+   }
+
+   componentDidMount() {
+      axios.get("https://photo-effect-backend.herokuapp.com/api/users")
+      .then(res => this.setState({testUsers: res.data}))
+      .catch(err => console.log(err))
+    }
+  
+    render() {
+      return (
+        <div>
+           <p>Users</p>
+          {this.state.testUsers.map(data => {
+            return (
+              <div>
+                {data.id}
+                {data.username}
+                {data.password}
+              </div>
+            )
+          })}
+        </div>
+      );
+    }
+}
+
+export default Users;
